e9350efea8d2726b899fc253434f48d1227564a0
Bikesh Shrestha <bikesh@sharelookapp.com>
2023-07-29 22:50:29 +0545

    chagnes

--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@ module.exports = withPWA({
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
   },
-  export: "next export"
+  output: 'export',
+  images: { unoptimized: true } 
   // };
 });
